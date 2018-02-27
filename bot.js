96145a240326bf0c0fc0b3c473703c4ac4cac7ab
Wildboylee <p235@philipmorant.essex.sch.uk>
2018-02-27 21:01:40 +0000

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,214 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const request = require("request");
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', message => {
+				var msg = message.toString();
+				outerloop:
+				for (var i = 0; i < msg.length; i++) {
+								if (msg[i] + msg[i+1] + msg[i+2] + msg[i+3] + msg[i+4] + msg[i+5] + msg[i+6] + msg[i+7] + msg[i+8] + msg[i+9] === "discord.gg")  {
+												console.log(msg + " Deleted!");
+												message.reply("don\'t advertise!");
+												message.delete();
+												break;
+								}
+								if (msg[i] + msg[i+1] + msg[i+2] + msg[i+3] + msg[i+4] + msg[i+5] + msg[i+6] + msg[i+7] + msg[i+8] + msg[i+9] + msg[i+10] + msg[i+11] + msg[i+12] + msg[i+13] + msg[i+14] + msg[i+15] + msg[i+16] + msg[i+17] + msg[i+18] + msg[i+19] + msg[i+20] === "discordapp.com/invite")  { //21
+												console.log(msg + " Deleted!");
+												message.reply("don\'t advertise!");
+												message.delete();
+												break;
+								}
+								if (msg[i] + msg[i+1] + msg[i+2] === "gg/")  {
+												console.log(msg + " Deleted!");
+												message.reply("don\'t advertise!");
+												message.delete();
+												break;
+								}
+								if (msg[i] + msg[i+1] + msg[i+2] + msg[i+3] + msg[i+4] + msg[i+5] + msg[i+6] === "/invite")  {
+												console.log(msg + " Deleted!");
+												message.reply("don\'t advertise!");
+												message.delete();
+												break;
+								}
+								
+								
+								
+								
+}
+});
+
+
+
+
+client.on('messageUpdate', (omsg, nmsg) => {
+  var msg = nmsg.toString();
+				for (var i = 0; i < msg.length; i++) {
+								if (msg[i] + msg[i+1] + msg[i+2] + msg[i+3] + msg[i+4] + msg[i+5] + msg[i+6] + msg[i+7] + msg[i+8] + msg[i+9] === "discord.gg")  {
+												console.log(msg + " Deleted!");
+												nmsg.reply("don\'t advertise!");
+												nmsg.delete();
+												break;
+								}
+								if (msg[i] + msg[i+1] + msg[i+2] + msg[i+3] + msg[i+4] + msg[i+5] + msg[i+6] + msg[i+7] + msg[i+8] + msg[i+9] + msg[i+10] + msg[i+11] + msg[i+12] + msg[i+13] + msg[i+14] + msg[i+15] + msg[i+16] + msg[i+17] + msg[i+18] + msg[i+19] + msg[i+20] === "discordapp.com/invite")  { //21
+												console.log(msg + " Deleted!");
+												nmsg.reply("don\'t advertise!");
+												nmsg.delete();
+												break;
+								}
+								if (msg[i] + msg[i+1] + msg[i+2] === "gg/")  {
+												console.log(msg + " Deleted!");
+												nmsg.reply("don\'t advertise!");
+												nmsg.delete();
+												break;
+								}
+								if (msg[i] + msg[i+1] + msg[i+2] + msg[i+3] + msg[i+4] + msg[i+5] + msg[i+6] === "/invite")  {
+												console.log(msg + " Deleted!");
+												nmsg.reply("don\'t advertise!");
+												nmsg.delete();
+												break;
+								}
+}
+});
+
+
+
+
+
+
+function testRedirect(url) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function(e) {
+    if (xhr.status == 200 && xhr.readyState == 4) {
+      if (url != xhr.responseURL) {
+        console.log("redirect detected to: " + xhr.responseURL)
+      } else {
+        console.log("no redirect detected")
+
+      }
+    }
+  }
+  xhr.open("GET", url, true);
+  xhr.send();
+}
+
+
+
+
+
+
+
+const { Client } = require('discord.js');
+const yt = require('ytdl-core');
+const tokens = require('./tokens.json');
+
+let queue = {};
+
+const commands = {
+	'play': (msg) => {
+		if (queue[msg.guild.id] === undefined) return msg.channel.sendMessage(`Add some songs to the queue first with ${tokens.prefix}add`);
+		if (!msg.guild.voiceConnection) return commands.join(msg).then(() => commands.play(msg));
+		if (queue[msg.guild.id].playing) return msg.channel.sendMessage('Already Playing');
+		let dispatcher;
+		queue[msg.guild.id].playing = true;
+
+		console.log(queue);
+		(function play(song) {
+			console.log(song);
+			if (song === undefined) return msg.channel.sendMessage('Queue is empty').then(() => {
+				queue[msg.guild.id].playing = false;
+				msg.member.voiceChannel.leave();
+			});
+			msg.channel.sendMessage(`Playing: **${song.title}** as requested by: **${song.requester}**`);
+			dispatcher = msg.guild.voiceConnection.playStream(yt(song.url, { audioonly: true }), { passes : tokens.passes });
+			let collector = msg.channel.createCollector(m => m);
+			collector.on('message', m => {
+				if (m.content.startsWith(tokens.prefix + 'pause')) {
+					msg.channel.sendMessage('paused').then(() => {dispatcher.pause();});
+				} else if (m.content.startsWith(tokens.prefix + 'resume')){
+					msg.channel.sendMessage('resumed').then(() => {dispatcher.resume();});
+				} else if (m.content.startsWith(tokens.prefix + 'skip')){
+					msg.channel.sendMessage('skipped').then(() => {dispatcher.end();});
+				} else if (m.content.startsWith('volume+')){
+					if (Math.round(dispatcher.volume*50) >= 100) return msg.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
+					dispatcher.setVolume(Math.min((dispatcher.volume*50 + (2*(m.content.split('+').length-1)))/50,2));
+					msg.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
+				} else if (m.content.startsWith('volume-')){
+					if (Math.round(dispatcher.volume*50) <= 0) return msg.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
+					dispatcher.setVolume(Math.max((dispatcher.volume*50 - (2*(m.content.split('-').length-1)))/50,0));
+					msg.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
+				} else if (m.content.startsWith(tokens.prefix + 'time')){
+					msg.channel.sendMessage(`time: ${Math.floor(dispatcher.time / 60000)}:${Math.floor((dispatcher.time % 60000)/1000) <10 ? '0'+Math.floor((dispatcher.time % 60000)/1000) : Math.floor((dispatcher.time % 60000)/1000)}`);
+				}
+			});
+			dispatcher.on('end', () => {
+				collector.stop();
+				play(queue[msg.guild.id].songs.shift());
+			});
+			dispatcher.on('error', (err) => {
+				return msg.channel.sendMessage('error: ' + err).then(() => {
+					collector.stop();
+					play(queue[msg.guild.id].songs.shift());
+				});
+			});
+		})(queue[msg.guild.id].songs.shift());
+	},
+	'join': (msg) => {
+		return new Promise((resolve, reject) => {
+			const voiceChannel = msg.member.voiceChannel;
+			if (!voiceChannel || voiceChannel.type !== 'voice') return msg.reply('I couldn\'t connect to your voice channel...');
+			voiceChannel.join().then(connection => resolve(connection)).catch(err => reject(err));
+		});
+	},
+	'add': (msg) => {
+		let url = msg.content.split(' ')[1];
+		if (url == '' || url === undefined) return msg.channel.sendMessage(`You must add a YouTube video url, or id after ${tokens.prefix}add`);
+		yt.getInfo(url, (err, info) => {
+			if(err) return msg.channel.sendMessage('Invalid YouTube Link: ' + err);
+			if (!queue.hasOwnProperty(msg.guild.id)) queue[msg.guild.id] = {}, queue[msg.guild.id].playing = false, queue[msg.guild.id].songs = [];
+			queue[msg.guild.id].songs.push({url: url, title: info.title, requester: msg.author.username});
+			msg.channel.sendMessage(`added **${info.title}** to the queue`);
+		});
+	},
+	'queue': (msg) => {
+		if (queue[msg.guild.id] === undefined) return msg.channel.sendMessage(`Add some songs to the queue first with ${tokens.prefix}add`);
+		let tosend = [];
+		queue[msg.guild.id].songs.forEach((song, i) => { tosend.push(`${i+1}. ${song.title} - Requested by: ${song.requester}`);});
+		msg.channel.sendMessage(`__**${msg.guild.name}'s Music Queue:**__ Currently **${tosend.length}** songs queued ${(tosend.length > 15 ? '*[Only next 15 shown]*' : '')}\n\`\`\`${tosend.slice(0,15).join('\n')}\`\`\``);
+	},
+	'help': (msg) => {
+		let tosend = ['```xl', tokens.prefix + 'join : "Join Voice channel of msg sender"',	tokens.prefix + 'add : "Add a valid youtube link to the queue"', tokens.prefix + 'queue : "Shows the current queue, up to 15 songs shown."', tokens.prefix + 'play : "Play the music queue if already joined to a voice channel"', '', 'the following commands only function while the play command is running:'.toUpperCase(), tokens.prefix + 'pause : "pauses the music"',	tokens.prefix + 'resume : "resumes the music"', tokens.prefix + 'skip : "skips the playing song"', tokens.prefix + 'time : "Shows the playtime of the song."',	'volume+(+++) : "increases volume by 2%/+"',	'volume-(---) : "decreases volume by 2%/-"',	'```'];
+		msg.channel.sendMessage(tosend.join('\n'));
+	},
+	'reboot': (msg) => {
+		if (msg.author.id == tokens.adminID) process.exit(); //Requires a node module like Forever to work.
+	}
+				
+				
+};
+
+client.on('ready', () => {
+	console.log('ready!');
+});
+
+client.on('message', msg => {
+	if (!msg.content.startsWith(tokens.prefix)) return;
+	if (commands.hasOwnProperty(msg.content.toLowerCase().slice(tokens.prefix.length).split(' ')[0])) commands[msg.content.toLowerCase().slice(tokens.prefix.length).split(' ')[0]](msg);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
