8aee553f5330c459bdbfe461a6ae0dac8aafc770
Wildboylee <p235@philipmorant.essex.sch.uk>
2018-03-02 12:49:05 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const client = new Discord.Client();
 const request = require("request");
 
 client.on('ready', () => {
@@ -89,23 +90,23 @@ const { Client, Util } = require('discord.js');
 const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
 
-const client = new Client({ disableEveryone: true });
+const client2 = new Client({ disableEveryone: true });
 
 const youtube = new YouTube(process.env.GOOGLE_API_KEY);
 
 const queue = new Map();
 
-client.on('warn', console.warn);
+client2.on('warn', console.warn);
 
-client.on('error', console.error);
+client2.on('error', console.error);
 
-client.on('ready', () => console.log('Yo this ready!'));
+client2.on('ready', () => console.log('Yo this ready!'));
 
-client.on('disconnect', () => console.log('I just disconnected, making sure you know, I will reconnect now...'));
+client2.on('disconnect', () => console.log('I just disconnected, making sure you know, I will reconnect now...'));
 
-client.on('reconnecting', () => console.log('I am reconnecting now!'));
+client2.on('reconnecting', () => console.log('I am reconnecting now!'));
 
-client.on('message', async msg => { // eslint-disable-line
+client2.on('message', async msg => { // eslint-disable-line
 	if (msg.author.bot) return undefined;
 	if (!msg.content.startsWith(process.env.PREFIX)) return undefined;
 
@@ -120,7 +121,7 @@ client.on('message', async msg => { // eslint-disable-line
 	if (command === 'play') {
 		const voiceChannel = msg.member.voiceChannel;
 		if (!voiceChannel) return msg.channel.send('I\'m sorry but you need to be in a voice channel to play music!');
-		const permissions = voiceChannel.permissionsFor(msg.client.user);
+		const permissions = voiceChannel.permissionsFor(msg.client2.user);
 		if (!permissions.has('CONNECT')) {
 			return msg.channel.send('I cannot connect to your voice channel, make sure I have the proper permissions!');
 		}
@@ -276,10 +277,6 @@ function play(guild, song) {
 
 	serverQueue.textChannel.send(`🎶 Start playing: **${song.title}**`);
 }
-
-
-
-
 
 
 
