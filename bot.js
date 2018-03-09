6c1a799874859f49cb0684639ccd05bc1b2a88c4
Wildboylee <p235@philipmorant.essex.sch.uk>
2018-03-09 21:48:33 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -269,7 +269,7 @@ function play(guild, song) {
 			play(guild, serverQueue.songs[0]);
 		})
 		.on('error', error => console.error(error));
-	dispatcher.setVolumeLogarithmic(serverQueue.volume / 1);
+	dispatcher.setVolumeLogarithmic(serverQueue.volume / 10);
 
 	serverQueue.textChannel.send(`🎶 Start playing: **${song.title}**`);
 }
