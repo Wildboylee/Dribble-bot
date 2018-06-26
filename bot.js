bce3d3a68c53260ce00d14810f58ed47ddae40c6
Wildboylee <p235@philipmorant.essex.sch.uk>
2018-06-26 04:00:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -151,7 +151,7 @@ client.on('message', async msg => { // eslint-disable-line
 });
   }
 	if(command === "lockdown") {
-		if (msg.author.id !== '191997658028572673' || msg.member.roles.some(r=>["Owners", "Retired Owner", "Admin"].includes(r.name)) ) return;
+		if (msg.author.id !== '191997658028572673' || !msg.member.roles.some(r=>["Owners", "Retired Owner", "Admin"].includes(r.name)) ) return;
 		msg.reply("has initiated a lockdown!(not really, this is a test)");
 		let role = message.guild.roles.find("name", "Muted");
 		if (msg.member.roles.has('457281418293411882')) {
