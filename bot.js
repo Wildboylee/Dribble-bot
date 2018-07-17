871ecd104fcc1df89500dbe530585abc80172f33
Wildboylee <p235@philipmorant.essex.sch.uk>
2018-07-17 23:00:43 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -168,11 +168,12 @@ client.on('message', async msg => { // eslint-disable-line
     const deleteCount = parseInt(args[0], 10);
     
     // Ooooh nice, combined conditions. <3
-    
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return msg.reply("Please provide a number between 2 and 100 for the number of messages to delete");
     
     // So we get our messages, and delete them. Simple enough, right?
     const fetched = await msg.channel.fetchMessages({limit: deleteCount});
-    message.channel.bulkDelete(fetched)
+    msg.channel.bulkDelete(fetched)
       .catch(error => msg.reply(`Couldn't delete messages because of: ${error}`));
   }
 	
